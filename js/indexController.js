033d797d541bae94c6b24a27cfc7919aef819e69
Stefan Mihajlovic <stef.mihajlovicc@gmail.com>
2023-06-03 11:34:17 +0200

    da

--- js/indexController.js
+++ js/indexController.js
@@ -284,7 +284,7 @@ console.log();
 
     let randomList = [];
 
-    for (let i = 0; i < 7; i++) {
+    for (let i = 0; i < 10; i++) {
         while(true){
             let g = Math.floor(Math.random() * brojArtista) + 1;
             if(!randomList.includes(g)){
