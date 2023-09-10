3c3c3d0244a70d5b77d15585dd08537c04c72cd8
Stefan Mihajlovic <stef.mihajlovicc@gmail.com>
2023-09-10 13:42:14 +0200

    Quick IOS Test

--- js/index.js
+++ js/index.js
@@ -686,7 +686,7 @@ screenScrollables.forEach((screen) => {
             if(screen.id != "screenScrollableCat"){
                 if(screen.scrollTop < 0){
                     screen.children[2].children[0].classList.add('noAnimTransitions');
-                    let newHeight = getComputedStyle(document.documentElement).getPropertyValue("--topInsetArea") - screen.scrollTop;
+                    let newHeight = getComputedStyle(document.documentElement).getPropertyValue("--topInsetArea") - screen.scrollTop + 400;
                     screen.children[2].children[0].style.height = `${newHeight}px`;
                 }else{
                     screen.children[2].children[0].classList.remove('noAnimTransitions');
