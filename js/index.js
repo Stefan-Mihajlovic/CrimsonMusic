63a7737f807fde28f62e3027d0143c026bc3ffbf
Stefan Mihajlovic <stef.mihajlovicc@gmail.com>
2023-09-10 14:45:23 +0200

    General Improvements - 2.6a
    - Made the side screen a little bit bigger

--- js/index.js
+++ js/index.js
@@ -686,11 +686,11 @@ screenScrollables.forEach((screen) => {
             if(screen.id != "screenScrollableCat"){
                 if(screen.scrollTop < 0){
                     screen.children[2].children[0].classList.add('noAnimTransitions');
-                    let newHeight = Number(-screen.scrollTop) + (400 + Number(getComputedStyle(document.documentElement).getPropertyValue("--topInsetArea").split('p')[0]));
+                    let newHeight = Number(-screen.scrollTop) + (500 + Number(getComputedStyle(document.documentElement).getPropertyValue("--topInsetArea").split('p')[0]));
                     screen.children[2].children[0].style.height = `${newHeight}px`;
                 }else{
                     screen.children[2].children[0].classList.remove('noAnimTransitions');
-                    screen.children[2].children[0].style.height = `calc(env(safe-area-inset-top) + 400px)`;
+                    screen.children[2].children[0].style.height = `calc(env(safe-area-inset-top) + 500px)`;
                 }
 
                 if(screen.scrollTop > 130){
