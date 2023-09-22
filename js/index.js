31abfaa89d07949608043e27d6e11a119f6fc4a6
Stefan Mihajlovic <stef.mihajlovicc@gmail.com>
2023-09-22 18:47:10 +0200

    Quick IOS Test

--- js/index.js
+++ js/index.js
@@ -716,7 +716,7 @@ screenScrollables.forEach((screen) => {
                     screen.children[2].children[0].classList.add('noAnimTransitions');
                     let newHeight = Number(-screen.scrollTop) + (500 + Number(getComputedStyle(document.documentElement).getPropertyValue("--topInsetArea").split('p')[0]));
                     screen.children[2].children[0].style.height = `${newHeight}px`;
-                    screen.children[2].children[1].style.opacity = (-screen.scrollTop / 50);
+                    screen.children[2].children[1].style.opacity = (50 / (-screen.scrollTop));
                 }else{
                     screen.children[2].children[1].style.opacity = 1;
                     screen.children[2].children[0].classList.remove('noAnimTransitions');
