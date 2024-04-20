d01af33284ef63c8e24fc52b4cee1333aa11a262
Stefan Mihajlovic <stef.mihajlovicc@gmail.com>
2024-04-20 15:28:21 +0200

    General Improvements - 0.2.7
    - Removed the colorful player (toggle and class)
    - Changed search screen so there is no sticky search bar
    - Other UI changes

--- js/index.js
+++ js/index.js
@@ -152,25 +152,6 @@ function clickEffect(button){
     }
 }
 
-const monopToggle = document.getElementById('monopToggle');
-let isMonopOn = false;
-let lastPlayerColor;
-monopToggle.addEventListener('click', () => {
-    isMonopOn = monopToggle.checked;
-    if(monopToggle.checked){
-        document.querySelector('.miniPlayer').classList.remove('colorfulMiniPlayer');
-        document.getElementsByClassName('songBackdrop')[0].style.display = 'none';
-        lastPlayerColor = getComputedStyle(document.documentElement).getPropertyValue('--currentSongColorBig');
-        document.documentElement.style.setProperty("--currentSongColorBig", 'rgb(28, 22, 37)');
-    }else{
-        document.querySelector('.miniPlayer').classList.add('colorfulMiniPlayer');
-        if(!isPerformanceModeOn){
-            document.getElementsByClassName('songBackdrop')[0].style.display = 'block';
-        }
-        document.documentElement.style.setProperty("--currentSongColorBig", lastPlayerColor);
-    }
-})
-
 let reduceAnimations = false;
 
 const redanimToggle = document.getElementById('redanimToggle');
@@ -299,9 +280,7 @@ function turnPerformanceModeOn(){
 function turnPerformanceModeOff(){
     document.documentElement.style.setProperty("--gmBackdrop", "blur(20px) brightness(1.2)");
     document.documentElement.style.setProperty("--gmBackdropPlayer", "brightness(0.5) blur(50px) saturate(2.5)");
-    if(!isMonopOn){
-        document.querySelector('.songBackdrop').style.display = 'block';
-    }
+    document.querySelector('.songBackdrop').style.display = 'block';
     pmToggle.checked = false;
     isPerformanceModeOn = false;
     redrawAppTheme();
